dc22c751d61e7181598fc29ff36e05824212afdb
Giulia Righi <righi.giulia@outlook.com>
2026-04-25 21:56:57 +0200

    Settimana 3(?)

--- girarifugi/rifugi/views.py
+++ girarifugi/rifugi/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group
+from django.core.paginator import Paginator
 from django import forms
+from .models import Rifugio
 
 # Form di registrazione con email
 class RegisterForm(UserCreationForm):
@@ -16,7 +18,6 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            # Assegna automaticamente al gruppo Escursionista
             gruppo = Group.objects.get(name='Escursionista')
             user.groups.add(gruppo)
             return redirect('/accounts/login/')
@@ -24,6 +25,46 @@ def register(request):
         form = RegisterForm()
     return render(request, 'registration/register.html', {'form': form})
 
-# Home page (per ora lista rifugi approvati)
+# Home page
 def home(request):
-    return render(request, 'home.html')
+    rifugi = Rifugio.objects.filter(stato='approvato')
+
+    # Ricerca
+    nome = request.GET.get('nome', '')
+    regione = request.GET.get('regione', '')
+    quota_min = request.GET.get('quota_min', '')
+    quota_max = request.GET.get('quota_max', '')
+
+    if nome:
+        rifugi = rifugi.filter(nome__icontains=nome)
+    if regione:
+        rifugi = rifugi.filter(regione__icontains=regione)
+    if quota_min:
+        rifugi = rifugi.filter(altitudine__gte=quota_min)
+    if quota_max:
+        rifugi = rifugi.filter(altitudine__lte=quota_max)
+
+    # Salva filtri in sessione
+    request.session['filtri'] = {
+        'nome': nome,
+        'regione': regione,
+        'quota_min': quota_min,
+        'quota_max': quota_max,
+    }
+
+    # Paginazione
+    paginator = Paginator(rifugi, 10)
+    page = request.GET.get('page')
+    rifugi_paginati = paginator.get_page(page)
+
+    return render(request, 'home.html', {
+        'rifugi': rifugi_paginati,
+        'nome': nome,
+        'regione': regione,
+        'quota_min': quota_min,
+        'quota_max': quota_max,
+    })
+
+def rifugio(request, pk):
+    r = Rifugio.objects.get(pk=pk)
+    return render(request, 'rifugi/rifugio.html', {'rifugio': r})
